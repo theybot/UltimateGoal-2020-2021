a3cad81888befda5915104e8348bea847b3b3f26
TheYbotS <clil.shoham@gmail.com>
2021-02-14 19:14:41 +0200

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomuses/TahelAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomuses/TahelAutoTest.java
@@ -16,14 +16,15 @@ public class TahelAutoTest extends RobotCustomade {
         RF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         RB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
+        MyDriveTrain.encoderDrive(0.5,25,-25,-25,25,1);
+        MyDriveTrain.encoderDrive(0.5,-100,-100,-100,-100,2);
+        MyDriveTrain.encoderDrive(0.5,-100,-100,-100,-100,2);
         telemetry.addData("LF:",LF.getCurrentPosition());
         telemetry.addData("LB:",LB.getCurrentPosition());
         telemetry.addData("RF:",RF.getCurrentPosition());
         telemetry.addData("RB:",RB.getCurrentPosition());
         telemetry.update();
-        MyDriveTrain.encoderDrive(0.5,25,-25,-25,25,1);
-        MyDriveTrain.encoderDrive(0.5,-100,-100,-100,-100,2);
-        MyDriveTrain.encoderDrive(0.5,-100,-100,-100,-100,2);
+        sleep(5000);
     }
 
 
